8fa9c0a2c4cf08d2c556c824ff2cdb86062e924e
gajeong <rlarkwjd127@gmail.com>
2021-09-16 17:09:23 +0900

    BOJ10845 작성 중

---
+++ gajeong/DataStructure/queue/BOJ10845.js
@@ -0,0 +1,43 @@
+/* Queue 구현 대표 예시  */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) =>{
+  const n = _input.shift();
+  const data = Array.from({length:10000},()=>-1)
+  let head=0;
+  let tail =1; 
+  let str = ""
+  for (let i=0;i<n;i++){
+    let nowComm = input[i].split(" ")
+    switch(newCommon[0]){
+      case "push":
+        break;
+      case "pop":
+        break;  
+      case "size":
+        break;  
+      case "empty":
+        break;  
+      case "front":
+        break;  
+      case "back":
+        break;  
+    }
+  }
+}
+
+const input = [];
+const main = (rl) => {
+  rl.on("line", (line) => {
+    input.push(line);
+  }).on("close", () => {
+    solution(input); // solution 함수를 만들어서 구현하면 됨! 
+    process.exit();
+  });
+};
+
+main(rl)
